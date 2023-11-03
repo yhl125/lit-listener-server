01ccc7707a94dd16f124e014cda243769183f434
Yoonhyoung-Lee <54613623+Yoonhyoung-Lee@users.noreply.github.com>
2023-11-03 16:33:26 +0900

    feat: add @nestjs/event-emitter

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    EventEmitterModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
