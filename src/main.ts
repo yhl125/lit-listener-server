d6d8001c01eacb4fe6203e3626ec3ade7f20634a
Yoonhyoung-Lee <54613623+Yoonhyoung-Lee@users.noreply.github.com>
2023-11-03 16:58:58 +0900

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,14 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('lit-listener-server')
+    .setDescription('lit-listener-server API')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3001);
 }
